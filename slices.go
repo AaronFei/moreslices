c65c4970cd6f816e94fb9a8240b312b4f87c7ba8
AaronFei <xyzyx760326@gmail.com>
2022-04-18 03:12:38 +0800

    Create slices.go

---
+++ slices.go
@@ -0,0 +1,24 @@
+package moreslices
+
+// Remove the element from slice if true
+func DeleteAll[E any](s []E, pFunc func(e E) bool) []E {
+	for i := 0; i < len(s); i++ {
+		if pFunc(s[i]) {
+			s = append(s[:i], s[i+1:]...)
+			i--
+		}
+	}
+	return s
+}
+
+func Insert[E any](s []E, e E, index int) []E {
+	if index > len(s) {
+		return nil
+	} else if index == len(s) {
+		s = append(s, e)
+	} else {
+		s = append(s[:index+1], s[index:]...)
+		s[index] = e
+	}
+	return s
+}
