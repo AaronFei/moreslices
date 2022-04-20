1d1b8c120c87897ebfd687863288ccd0b2285acb
AaronFei <xyzyx760326@gmail.com>
2022-04-20 10:29:43 +0800

    Update slices.go

--- slices.go
+++ slices.go
@@ -1,7 +1,7 @@
 package moreslices
 
 // Remove the element from slice if true
-func DeleteAll[E any](s []E, pFunc func(e E) bool) []E {
+func DeleteAll[S ~[]E, E any](s S, pFunc func(e E) bool) S {
 	for i := 0; i < len(s); i++ {
 		if pFunc(s[i]) {
 			s = append(s[:i], s[i+1:]...)
@@ -13,7 +13,7 @@ func DeleteAll[E any](s []E, pFunc func(e E) bool) []E {
 
 // Insert the element to specific index
 // return nil if index out of range
-func Insert[E any](s []E, e E, index int) []E {
+func Insert[S ~[]E, E any](s S, e E, index int) S {
 	if index > len(s) {
 		return nil
 	} else if index == len(s) {
